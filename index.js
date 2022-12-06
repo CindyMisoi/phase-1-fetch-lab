14eaf705aad40519545b35f4d85a4d1cf7158887
CindyMisoi <crmisoi2k@gmail.com>
2022-12-06 07:59:01 +0300

    called fetchBook() function

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
-  .then((res)=> res.json())
-  .then(function (books){
-    return renderBooks(books["names"])
-    
-  })
-  
+  .then(res=> res.json())
+  .then(books => books.forEach(book =>{
+    renderBooks(book)
+  }))
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(book) {
+const main = document.querySelector('main');
+const h2 = document.createElement('h2');
+h2.innerHTML = book.name;
+main.appendChild(h2);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks()
+})
+
